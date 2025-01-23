a72a9e57b54befde0d7d8cda0f897b454b290936
Anik Adhikari <developwithanik@gmail.com>
2025-01-23 17:18:14 +0530

    fix minor issues

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { exec } from "child_process";
+import { exec as execChildProcess } from "child_process";
 import fs from "fs";
 import inquirer from "inquirer";
 import ora from "ora";
@@ -8,15 +8,31 @@ import path from "path";
 import simpleGit from "simple-git";
 import { promisify } from "util";
 
-const execPromise = promisify(exec);
+const execPromise = promisify(execChildProcess);
 
 const REPO_URL = "https://github.com/AnikAdhikari7/express-setup.git";
 const BRANCH = "main";
 const DEPTH = 1;
 
+const removeGitFolder = async (targetPath) => {
+    return new Promise((resolve, reject) => {
+        fs.rm(
+            path.join(targetPath, ".git"),
+            { recursive: true, force: true },
+            (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            }
+        );
+    });
+};
+
 const init = async () => {
     console.log(
-        "\n🚀 Welcome to xpress-init! Setting up your Express app...\n"
+        "\n🤝 Welcome to xpress-init! Setting up your Express app...\n"
     );
 
     // Step 1: Prompt for the project name
@@ -25,7 +41,7 @@ const init = async () => {
             type: "input",
             name: "projectName",
             message: "Enter your project name: ",
-            default: "my-express-app",
+            default: "my-xpress-app",
         },
     ]);
 
@@ -40,17 +56,20 @@ const init = async () => {
     }
 
     // Step 3: Clone the repository
-    const spinner = ora("Cloning the repository...").start();
+    const spinner = ora("Initializing the project...").start();
     try {
         await simpleGit()
-            .silent(true)
             .clone(REPO_URL, projectName, [
                 "--branch",
                 BRANCH,
                 "--depth",
                 DEPTH,
             ]);
-        spinner.succeed("Repository cloned successfully!");
+        spinner.succeed("Project initialized successfully!");
+
+        // Remove the .git folder
+        await removeGitFolder(targetPath);
+        // spinner.succeed(".git folder removed successfully!");
 
         // Step 4: Install dependencies
         spinner.start("Installing dependencies...");
@@ -61,7 +80,7 @@ const init = async () => {
         console.log("\n🎉 Setup complete! Next steps: ");
         console.log(`  cd ${projectName}`);
         console.log("  npm start");
-        console.log("\nHappy coding! 🚀");
+        console.log("\nHappy eXpressing! 🚀\n");
     } catch (error) {
         spinner.fail("Failed to complete setup.");
         console.error(error.message);
